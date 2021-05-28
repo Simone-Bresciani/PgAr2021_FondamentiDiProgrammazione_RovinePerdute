16e9e830cf4618b8f9abef2c6738db5bacd1587d
simone bresciani <s.bresciani026@studenti.unibs.it>
2021-05-28 23:01:01 +0200

    aggiunta javadoc

--- src/it/unibs/arnaldo/rovinePerdute/Navigatore.java
+++ src/it/unibs/arnaldo/rovinePerdute/Navigatore.java
@@ -20,8 +20,6 @@ public class Navigatore {
      */
     public ArrayList<Luogo> trovaPercorso(Luogo luogo_partenza, Luogo luogo_arrivo, Veicolo veicolo){
         //creo una Queue che sarà il mio percorso
-//        Comparator<NodoEsteso> comparatore = new ComparatoreNodi();
-//        PriorityQueue<NodoEsteso> percorso_queue = new PriorityQueue<>(comparatore);
         ArrayList<NodoEsteso> percorso_array = new ArrayList<>();
         //creo una Map che associa al Luogo il suo NodoEsteso, ovvero il Luogo con le info aggiuntive
         //questa map salverà tutti i luoghi visitati fin ora
@@ -39,7 +37,6 @@ public class Navigatore {
             riordina(percorso_array);
             //prelevo il primo nodo dell'array
             NodoEsteso nodo_ripartenza = percorso_array.get(0);
-            System.out.println(percorso_array.get(0).getLuogo_corrente().getId());
             percorso_array.remove(0);
             //controllo che il luogo selezionato corrisponda al luogo di arrivo
             if(nodo_ripartenza.getLuogo_corrente().equals(luogo_arrivo)){
@@ -57,9 +54,9 @@ public class Navigatore {
             }
             //salvo il luogo del nodo da dove ero ripartito(ovvero il suo luogo esteso)
             Luogo luogo_nodo_ripartenza = nodo_ripartenza.getLuogo_corrente();
-
+            //creo un set dei luoghi connessi al nodo di ripartenza
             Set<Luogo> luoghi_connessi_al_nodo_ripartenza = grafo.getArchi(luogo_nodo_ripartenza);
-
+            //ciclo tutte le connessioni al nodo di ripartenza
             for(Luogo luogo_connesso : luoghi_connessi_al_nodo_ripartenza){
                 //estendo il luogo appena analizzato nei luoghi connessi al luogo di ripartenza
                 NodoEsteso nodo_esteso_default = new NodoEsteso(luogo_connesso);
@@ -67,7 +64,9 @@ public class Navigatore {
                 NodoEsteso nodo_connesso = nodi_visitati.getOrDefault(luogo_connesso, nodo_esteso_default);
                 //aggiunta del nodo visitato
                 nodi_visitati.put(luogo_connesso, nodo_connesso);
+                //calcolo il carburante tra il nodo di ripartenza e il luogo connesso
                 double carburante_tappa_singola =  calcolaCarburante(nodo_ripartenza.getLuogo_corrente(), luogo_connesso, veicolo.getTipologia());
+                //calcolo il carburante che ho speso dalla partenza al luogo connesso
                 double carburante_fino_connessione = nodo_ripartenza.getCarburante_utilizzato() + carburante_tappa_singola;
 
                 /*se sono già arrivato precedentemente al nodo connesso utilizzando meno carburante, allora non sarà la strada giusta
@@ -80,6 +79,7 @@ public class Navigatore {
                     nodo_connesso.setCarburante_utilizzato(carburante_fino_connessione);
                     nodo_connesso.setCarburante_stimato(carburante_fino_connessione + calcolaCarburante(luogo_connesso, luogo_arrivo, veicolo.getTipologia()));
                     int id_nodo_prec = nodo_connesso.getLuogo_corrente().getId();
+                    //se é giá presente questo luogo lo tolgo e lo reinserisco, cosí non avró doppioni nell'arraylist
                     for (int i=0; i<percorso_array.size(); i++){
                         if (percorso_array.get(i).getLuogo_corrente().getId() == id_nodo_prec){
                             percorso_array.remove(percorso_array.get(i));
@@ -124,6 +124,12 @@ public class Navigatore {
         divide(0, array_nodi.size()-1, array_nodi);
     }
 
+    /**
+     * <h3>Metodo per l'ordinamento dell'array che divide la lista fino ai singoli elementi </h3>
+     * @param startIndex l'elemento iniziale
+     * @param endIndex l'elemento finale
+     * @param array_nodi ovvero i nodi dell'array
+     */
     private static void divide(int startIndex,int endIndex, ArrayList<NodoEsteso> array_nodi){
         //Divide till you breakdown your list to single element
         if(startIndex<endIndex && (endIndex-startIndex)>=1){
@@ -134,9 +140,17 @@ public class Navigatore {
             merger(startIndex,mid,endIndex, array_nodi);
         }
     }
+
+    /**
+     * <h3>Metodo per l'ordinamento dell'array che riassembla l'array</h3>
+     * @param startIndex valore iniziale
+     * @param midIndex valore intermedio
+     * @param endIndex valore finale
+     * @param array_nodi ovvero i nodi dell'array
+     */
     private static void merger(int startIndex,int midIndex,int endIndex, ArrayList<NodoEsteso> array_nodi){
 
-        //Below is the mergedarray that will be sorted array Array[i-midIndex] , Array[(midIndex+1)-endIndex]
+        //Below is the merged array that will be sorted array Array[i-midIndex] , Array[(midIndex+1)-endIndex]
         ArrayList<NodoEsteso> mergedSortedArray = new ArrayList<NodoEsteso>();
 
         int leftIndex = startIndex;
